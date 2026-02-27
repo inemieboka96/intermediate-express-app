a7adce334a44134bf7ebb92965bb9f7e18a61ea6
inemieboka96 <inemieboka96@gmail.com>
2026-02-27 10:43:59 +0100

    Implemented a product's router & some sample user & products data

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-import userRoutes from "./routes/users";
+// User routes frmo 
+import userRoutes from "./routes/users.js";
 
 // Use PORT 4000 in case of any errors
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -13,8 +14,8 @@ app.use("/users", userRoutes);
 
 // Error Handling
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something Broke!!!");
+  console.error(err.stack); // Log any errors
+  res.status(500).send("Something Broke!!!"); // Internal Server Error
 });
 
 // Async Error Handler (Always wrap async errors in try...catches)
